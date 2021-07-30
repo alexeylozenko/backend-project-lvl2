291c15976963c03e747dbd76556e1ca347ee5651
alexeylozenko <alexeylozenko@gmail.com>
2021-07-30 20:22:56 +0300

    refactored tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -12,34 +12,40 @@ const __dirname = path.dirname(__filename);
 const getFixturesPath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturesPath(filename), 'utf-8');
 
+
+
+const flatjson1 = getFixturesPath('flat-tree1.json');
+const flatjson2 = getFixturesPath('flat-tree2.json');
+
+const nestedjson1 = getFixturesPath('nested-tree1.json');
+const nestedjson2 = getFixturesPath('nested-tree2.json');
+
+const flatyaml1 = getFixturesPath('flat-tree1.yaml');
+const flatyaml2 = getFixturesPath('flat-tree2.yml');
+
+const nestedyaml1 = getFixturesPath('nested-tree1.yaml');
+const nestedyaml2 = getFixturesPath('nested-tree2.yml');
+
 describe('stylish', () => {
 
 	test('flat json', () => {
-		const filename1 = getFixturesPath('flat-tree1.json');
-		const filename2 = getFixturesPath('flat-tree2.json');
 		const result = readFile('flat-stylish.txt');
-		expect(genDiff(filename1, filename2)).toBe(result);
+		expect(genDiff(flatjson1, flatjson2)).toBe(result);
 	});
 
 	test('nested json', () => {
-		const filename1 = getFixturesPath('nested-tree1.json');
-		const filename2 = getFixturesPath('nested-tree2.json');
 		const result = readFile('nested-stylish.txt');
-		expect(genDiff(filename1, filename2)).toBe(result);
+		expect(genDiff(nestedjson1, nestedjson2)).toBe(result);
 	})
 
 	test('flat yaml', () => {
-		const filename1 = getFixturesPath('flat-tree1.yaml');
-		const filename2 = getFixturesPath('flat-tree2.yml');
 		const result = readFile('flat-stylish.txt');
-		expect(genDiff(filename1, filename2)).toBe(result);
+		expect(genDiff(flatyaml1, flatyaml2)).toBe(result);
 	});
 
 	test('nested yaml', () => {
-		const filename1 = getFixturesPath('nested-tree1.yaml');
-		const filename2 = getFixturesPath('nested-tree2.yml');
 		const result = readFile('nested-stylish.txt');
-		expect(genDiff(filename1, filename2)).toBe(result);
+		expect(genDiff(nestedyaml1, nestedyaml2)).toBe(result);
 	})
 
 });
@@ -47,31 +53,23 @@ describe('stylish', () => {
 describe('plain', () => {
 
 	test('flat json', () => {
-		const filename1 = getFixturesPath('flat-tree1.json');
-		const filename2 = getFixturesPath('flat-tree2.json');
 		const result = readFile('flat-plain.txt');
-		expect(genDiff(filename1, filename2, 'plain')).toBe(result);
+		expect(genDiff(flatjson1, flatjson2, 'plain')).toBe(result);
 	});
 
 	test('nested json', () => {
-		const filename1 = getFixturesPath('nested-tree1.json');
-		const filename2 = getFixturesPath('nested-tree2.json');
 		const result = readFile('nested-plain.txt');
-		expect(genDiff(filename1, filename2, 'plain')).toBe(result);
+		expect(genDiff(nestedjson1, nestedjson2, 'plain')).toBe(result);
 	});
 
 	test('flat yaml', () => {
-		const filename1 = getFixturesPath('flat-tree1.yaml');
-		const filename2 = getFixturesPath('flat-tree2.yml');
 		const result = readFile('flat-plain.txt');
-		expect(genDiff(filename1, filename2, 'plain')).toBe(result);
+		expect(genDiff(flatyaml1, flatyaml2, 'plain')).toBe(result);
 	});
 
 	test('nested yaml', () => {
-		const filename1 = getFixturesPath('nested-tree1.yaml');
-		const filename2 = getFixturesPath('nested-tree2.yml');
 		const result = readFile('nested-plain.txt');
-		expect(genDiff(filename1, filename2, 'plain')).toBe(result);
+		expect(genDiff(nestedyaml1, nestedyaml2, 'plain')).toBe(result);
 	})
 
 });
@@ -79,31 +77,23 @@ describe('plain', () => {
 describe('json', () => {
 	
 	test('flat json', () => {
-		const filename1 = getFixturesPath('flat-tree1.json');
-		const filename2 = getFixturesPath('flat-tree2.json');
 		const result = readFile('flat-json.txt');
-		expect(genDiff(filename1, filename2, 'json')).toBe(result);
+		expect(genDiff(flatjson1, flatjson2, 'json')).toBe(result);
 	});
 
 	test('nested json', () => {
-		const filename1 = getFixturesPath('nested-tree1.json');
-		const filename2 = getFixturesPath('nested-tree2.json');
 		const result = readFile('nested-json.txt');
-		expect(genDiff(filename1, filename2, 'json')).toBe(result);
+		expect(genDiff(nestedjson1, nestedjson2, 'json')).toBe(result);
 	});
 
 	test('flat yaml', () => {
-		const filename1 = getFixturesPath('flat-tree1.yaml');
-		const filename2 = getFixturesPath('flat-tree2.yml');
 		const result = readFile('flat-json.txt');
-		expect(genDiff(filename1, filename2, 'json')).toBe(result);
+		expect(genDiff(flatyaml1, flatyaml2, 'json')).toBe(result);
 	});
 
 	test('nested yaml', () => {
-		const filename1 = getFixturesPath('nested-tree1.yaml');
-		const filename2 = getFixturesPath('nested-tree2.yml');
 		const result = readFile('nested-json.txt');
-		expect(genDiff(filename1, filename2, 'json')).toBe(result);
+		expect(genDiff(nestedyaml1, nestedyaml2, 'json')).toBe(result);
 	});
 
 });
