b2aa0fd0f649d3aed09f9b99aef9e457ba100b07
alexeylozenko <alexeylozenko@gmail.com>
2021-08-22 15:06:34 +0300

    refactored src/parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,16 @@
 import yaml from 'js-yaml';
 
+const mapping = {
+	'.json' : JSON.parse,
+	'.yaml' : yaml.load,
+	'.yml' : yaml.load,
+}
+
+const chooseParser = (extname) => mapping[extname];
+
 const parse = (data, extname) => {
-  if (extname === '.yaml' || extname === '.yml') {
-    return yaml.load(data);
-  }
-  return JSON.parse(data);
-};
+	const parseData = chooseParser(extname);
+	return parseData(data);
+}
 
 export default parse;
