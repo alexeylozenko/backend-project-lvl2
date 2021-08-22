89ff657fd57a0021ca7521cf7df01ff6c1100d0b
alexeylozenko <alexeylozenko@gmail.com>
2021-08-22 15:09:57 +0300

    fixed linter errors

--- src/parsers.js
+++ src/parsers.js
@@ -1,16 +1,16 @@
 import yaml from 'js-yaml';
 
 const mapping = {
-	'.json' : JSON.parse,
-	'.yaml' : yaml.load,
-	'.yml' : yaml.load,
-}
+  '.json': JSON.parse,
+  '.yaml': yaml.load,
+  '.yml': yaml.load,
+};
 
 const chooseParser = (extname) => mapping[extname];
 
 const parse = (data, extname) => {
-	const parseData = chooseParser(extname);
-	return parseData(data);
-}
+  const parseData = chooseParser(extname);
+  return parseData(data);
+};
 
 export default parse;
