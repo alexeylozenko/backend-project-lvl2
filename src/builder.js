a96ff175e95fe3135ebde52e27e26fc66a457091
alexeylozenko <alexeylozenko@gmail.com>
2021-08-25 12:50:05 +0300

    update builder tree

--- src/builder.js
+++ src/builder.js
@@ -2,16 +2,16 @@ import _ from 'lodash';
 
 const buildTree = (data1, data2) => {
   const keys = _.union(Object.keys(data1), Object.keys(data2));
-  return _.sortBy(keys, (el) => el)
+  return _.sortBy(keys)
     .map((key) => {
       if (!_.has(data2, key)) {
         return {
-          type: 'removed', key, value1: data1[key], value2: null, children: null,
+          type: 'removed', key, value1: data1[key], 
         };
       }
       if (!_.has(data1, key)) {
         return {
-          type: 'added', key, value1: null, value2: data2[key], children: null,
+          type: 'added', key, value2: data2[key],
         };
       }
       if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
@@ -19,13 +19,13 @@ const buildTree = (data1, data2) => {
           type: 'changed', key, value1: data1[key], value2: data2[key], children: buildTree(data1[key], data2[key]),
         };
       }
-      if (data1[key] !== data2[key]) {
+      if (!_.isEqual(data1[key], data2[key])) {
         return {
-          type: 'updated', key, value1: data1[key], value2: data2[key], children: null,
+          type: 'updated', key, value1: data1[key], value2: data2[key],
         };
       }
       return {
-        type: 'unchanged', key, value1: data1[key], value2: data2[key], children: null,
+        type: 'unchanged', key, value1: data1[key], value2: data2[key],
       };
     });
 };
