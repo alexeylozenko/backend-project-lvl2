1375b70bf1ec0ea128decdbdbe4a9034d5baa65c
alexeylozenko <alexeylozenko@gmail.com>
2021-09-12 21:33:13 +0300

    updated stylish formatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,17 +1,13 @@
 import _ from 'lodash';
 
-const indent = (depth, mark = '  ') => ((depth !== 0)
-  ? '  '.repeat(depth * 2 - 1) + mark
-  : '');
+const indent = (depth, quantity = 4) => ' '.repeat(depth * quantity);
 
 const stringify = (data, depth) => {
-  const currentIndent = indent(depth);
-  const newIndent = indent(depth + 1);
   const objectToString = (obj) => {
     const lines = Object.entries(obj)
-      .map(([key, value]) => `${newIndent}${key}: ${stringify(value, depth + 1)}`);
+      .map(([key, value]) => `${indent(depth)}${key}: ${stringify(value, depth + 1)}`);
 
-    return ['{', ...lines, `${currentIndent}}`].join('\n');
+    return ['{', ...lines, `${indent(depth - 1)}}`].join('\n');
   };
   if (_.isPlainObject(data)) {
     return `${objectToString(data, depth)}`;
@@ -21,29 +17,28 @@ const stringify = (data, depth) => {
 
 const formatStylish = (diffTree) => {
   const format = (tree, depth) => {
-    const currentIndent = indent(depth);
     const lines = tree.map((node) => {
       switch (node.type) {
         case 'added':
-          return `${indent(depth + 1, '+ ')}${node.key}: ${stringify(node.value2, depth + 1)}`;
+          return `${indent(depth - 1)}  + ${node.key}: ${stringify(node.value2, depth + 1)}`;
         case 'removed':
-          return `${indent(depth + 1, '- ')}${node.key}: ${stringify(node.value1, depth + 1)}`;
+          return `${indent(depth - 1)}  - ${node.key}: ${stringify(node.value1, depth + 1)}`;
         case 'changed':
-          return `${indent(depth + 1)}${node.key}: ${format(node.children, depth + 1)}`;
+          return `${indent(depth)}${node.key}: ${format(node.children, depth + 1)}`;
         case 'unchanged':
-          return `${indent(depth + 1)}${node.key}: ${stringify(node.value1, depth + 1)}`;
+          return `${indent(depth)}${node.key}: ${stringify(node.value1, depth + 1)}`;
         case 'updated':
           return [
-            `${indent(depth + 1, '- ')}${node.key}: ${stringify(node.value1, depth + 1)}`,
-            `${indent(depth + 1, '+ ')}${node.key}: ${stringify(node.value2, depth + 1)}`,
+            `${indent(depth - 1)}  - ${node.key}: ${stringify(node.value1, depth + 1)}`,
+            `${indent(depth - 1)}  + ${node.key}: ${stringify(node.value2, depth + 1)}`,
           ];
         default:
           throw new Error('invalid state data');
       }
     });
-    return ['{', ..._.flatten(lines), `${currentIndent}}`].join('\n');
+    return ['{', ..._.flatten(lines), `${indent(depth - 1)}}`].join('\n');
   };
-  return format(diffTree, 0);
+  return format(diffTree, 1);
 };
 
 export default formatStylish;
